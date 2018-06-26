6f7f0fd820b3ba9b2da1ceb4000b4689b3678e0c
Samantha Oliver <soliver@usgs.gov>
2018-06-25 21:41:10 -0500

    Modified remake so figures would generate. Seems like the issue was that the construct_flow_fig arguments weren't in order in the remake file, even though they were defined. Seems like that shouldn't matter - but it fixed issue.

--- 25_flow_history_eval/code/flow_history_fxns.R
+++ 25_flow_history_eval/code/flow_history_fxns.R
@@ -1,3 +1,8 @@
+create_list <- function(...) {
+  dat <- list(...)
+  return(dat)
+}
+
 construct_flow_fig <- function(dat, file_name, stat) {
   plot_titles <- c('Water Year', 'Winter', 'Spring', 'Summer')
   stat_titles <- c('1-day min', '7-day min', '30-day min', 'median', 'mean', '30-day max', '7-day max', '1-day max')
