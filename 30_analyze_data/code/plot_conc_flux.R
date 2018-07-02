2bdb6b7d7e2f96495dcc5252619bfee194379e32
Samantha Oliver <soliver@usgs.gov>
2018-07-02 10:48:55 -0500

    added flux figures. Changed to have png written within function because remake wasn't working to write figure in remake file. Check into why this wasn't working.

--- 30_analyze_data/code/plot_conc_flux.R
+++ 30_analyze_data/code/plot_conc_flux.R
@@ -1,18 +1,5 @@
-# series analysis results
-
-# table of caluclated conc/flux
-# tableResults(tp_spring_out)
-
-# plot of conc
-
-##plotConcHist(tp_spring_out)
-
-# plot of flux
-
-#plotFluxHist(tp_spring_out)
-
-construct_conc_fig <- function(mod_out, seasonal, constituent, file_name) {
-  if (seasonal == TRUE) {
+construct_conc_fig <- function(mod_out, type, constituent, file_name = target_name) {
+  if (type == 'seasonal') {
     
     plot_titles <- c('Winter', 'Spring', 'Summer', 'Fall')
     extra_text <- c('Conc (dots), FN Conc (line)')
@@ -23,12 +10,12 @@ construct_conc_fig <- function(mod_out, seasonal, constituent, file_name) {
     
     for (i in 1:length(mod_out)) {
       plotConcHist(mod_out[[i]], printTitle = F, customPar = T)
-      title(main = paste(plot_titles[i], constitudent, extra_text))
+      title(main = paste(plot_titles[i], constituent, extra_text))
     }
     
     dev.off()
     
-  } else {
+  } else if (type == 'wy'){
    
     plot_title = paste("Water Year", constituent, "Conc (dots), FN Conc (line)")
     
@@ -37,6 +24,40 @@ construct_conc_fig <- function(mod_out, seasonal, constituent, file_name) {
     par(cex = 1.2)
     
     plotConcHist(mod_out, printTitle = F, customPar = T)
+    title(main = plot_title)
+    
+    dev.off()
+    
+  }
+  
+}
+
+construct_flux_fig <- function(mod_out, type, constituent, file_name = target_name) {
+  if (type == 'seasonal') {
+    
+    plot_titles <- c('Winter', 'Spring', 'Summer', 'Fall')
+    extra_text <- c('Flux (dots), FN Flux (line)')
+    
+    png(file_name, height = 700, width = 700)
+    
+    par(mfrow = c(2,2), mar = c(2,5,4,1), cex = 1.2)
+    
+    for (i in 1:length(mod_out)) {
+      plotFluxHist(mod_out[[i]], printTitle = F, customPar = T)
+      title(main = paste(plot_titles[i], constituent, extra_text))
+    }
+    
+    dev.off()
+    
+  } else if (type == 'wy'){
+    
+    plot_title = paste("Water Year", constituent, "Flux (dots), FN Flux (line)")
+    
+    png(file_name, height = 400, width = 400)
+    
+    par(cex = 1.2)
+    
+    plotFluxHist(mod_out, printTitle = F, customPar = T)
     title(main = plot_title)
     
     dev.off()
